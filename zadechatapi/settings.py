8912e19ace2e5198ab35e235126193eb92914c16
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-06-15 23:54:57 +0530

    n

--- zadechatapi/settings.py
+++ zadechatapi/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure--71s+0qy$=j89ss*bw&9odwgmjquyg*85ymxty^$u7t8v3+i6_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'zadechatapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'verceldb',
-#         'USER': 'default',
-#         'PASSWORD': 'zU0cw2lGpNBA',
-#         'HOST': 'ep-steep-glade-805441-pooler.ap-southeast-1.aws.neon.tech',
-#         'PORT': '',
-        
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'zU0cw2lGpNBA',
+        'HOST': 'ep-steep-glade-805441-pooler.ap-southeast-1.aws.neon.tech',
+        'PORT': '',
+        
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
