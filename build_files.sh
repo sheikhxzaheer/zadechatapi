19302f4ca0297c6a0fec77cfd2f73635aa60190d
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-06-15 23:59:00 +0530

    n

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "BUILD START"
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
