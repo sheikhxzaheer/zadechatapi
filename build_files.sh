66e7317b94de909acce6339821dc2abc9e2d256e
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-06-15 17:33:29 +0530

    n

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.12 -m ensurepip --upgrade
+python3.12 -m pip install --upgrade pip
+pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput
 echo " BUILD END"
