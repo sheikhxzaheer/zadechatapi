9604d97303893b86a484ef9e50116f432036d195
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-06-16 15:28:23 +0530

    added status in code

--- chatapi/views.py
+++ chatapi/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from zadechatapi.utils import zResponse
 
 
 class ChatView(generics.ListCreateAPIView):
@@ -13,10 +13,10 @@ class ChatView(generics.ListCreateAPIView):
             queryset = self.get_queryset()
             serializer = self.get_serializer(queryset, many=True)
             data = serializer.data
-            return Response(status=200, data=data)
+            return zResponse(status=200, data=data)
         except Exception as e:
             error_message = str(e)
-            return Response(status=404, data=error_message)
+            return zResponse(status=404, data=error_message)
         
     def create(self, request):
         try:
@@ -24,10 +24,10 @@ class ChatView(generics.ListCreateAPIView):
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
             serializer
-            return Response(status=200 , data=serializer.data)
+            return zResponse(status=200, message="New Chat Creted")
 
         except Exception as e:
-            return Response(status=404, data=str(e))
+            return zResponse(status=404, data=str(e))
         
 
 class MessageView(generics.ListCreateAPIView):
@@ -39,10 +39,10 @@ class MessageView(generics.ListCreateAPIView):
             queryset = self.get_queryset()
             serializer = self.get_serializer(queryset, many=True)
             data = serializer.data
-            return Response(status=200, data=data)
+            return zResponse(status=200, data=data)
         except Exception as e:
             error_message = str(e)
-            return Response(status=404, data=error_message)
+            return zResponse(status=404, data=error_message)
         
     def create(self, request):
         try:
@@ -50,9 +50,9 @@ class MessageView(generics.ListCreateAPIView):
             serializer.is_valid(raise_exception=True)
             self.perform_create(serializer)
             serializer
-            return Response(status=200 , data=serializer.data)
+            return zResponse(status=200, data=serializer.data)
 
         except Exception as e:
-            return Response(status=404, data=str(e))
+            return zResponse(status=404, data=str(e))
 
 
