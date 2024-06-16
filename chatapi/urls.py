2c49d0ce00ab5208b6b48f09afafa776dc7cee67
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-06-16 23:41:47 +0530

    added message in

--- chatapi/urls.py
+++ chatapi/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path("chat/", ChatView.as_view()),
     path("message/", MessageView.as_view()),
-
+     path("chat/<int:pk>/", ChatDetails.as_view()),
 ]
--- chatapi/views.py
+++ chatapi/views.py
@@ -30,6 +30,26 @@ class ChatView(generics.ListCreateAPIView):
             return zResponse(status=404, data=str(e))
         
 
+class ChatDetails(generics.RetrieveAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer  
+    def retrieve(self, request, pk):     
+        try:
+            instance= self.get_object()
+            serializer = self.get_serializer(instance)
+            chat_data = serializer.data
+            
+            
+            messages = Message.objects.filter(chat=instance)
+            message_serializer = MessageSerializer(messages, many=True)
+            chat_data['messages'] = message_serializer.data
+            return zResponse(data=chat_data, status=200, message='Employee Details Found')
+        except Exception as e:
+            error_message = str(e)
+            return zResponse(status=500, errorMessage=error_message)
+
+    
+
 class MessageView(generics.ListCreateAPIView):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
