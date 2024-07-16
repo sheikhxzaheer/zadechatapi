343f3ecca8f2ad709d0a19aeb278cfcc9108610f
sheikhxzaheer <sheikhzaheer037@gmail.com>
2024-07-16 12:35:52 +0530

    n

--- chatapi/consumers.py
+++ chatapi/consumers.py
@@ -1,25 +1,27 @@
+import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
-
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'chat_{self.room_name}'
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
+
     
-    async def disconnect(self, close_code):
+    async def disconnect(self):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chat_message',  
-                'message': text_data
+                'type': 'send_data',
+                'incoming_message' : text_data
             }
         )
         
-    async def chat_message(self, event):
-        message = event['message']
-        await self.send(text_data=message)
+
+    async def send_data(self, event):
+        await self.send(text_data=json.dumps(event))
+    
+    
